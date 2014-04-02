6211268c41020332f596e079818a8bb99af9d408
Michael Kelsey <kelsey@slac.stanford.edu>
2014-04-01 23:25:17 -0700

    Generate one e/h pair per event; user may change using /gun/number

--- charge/src/ChargePrimaryGeneratorAction.cc
+++ charge/src/ChargePrimaryGeneratorAction.cc
@@ -1,26 +1,20 @@
+// $Id$
+//
+// Generator uses G4ParticleGun, producing one electron and one hole per
+// event by default.  User may change the number of particles per event
+// via macro command |/gun/number|
 
 #include "ChargePrimaryGeneratorAction.hh"
-
-#include "G4Event.hh"
-#include "G4ParticleGun.hh"
-#include "G4RandomDirection.hh"
-
-#include "G4PhononTransFast.hh"
-#include "G4PhononTransSlow.hh"
-#include "G4PhononLong.hh"
-
-#include "G4Electron.hh"
 #include "G4CMPDriftElectron.hh"
 #include "G4CMPDriftHole.hh"
+#include "G4ParticleGun.hh"
+#include "G4PhononLong.hh"
+#include "G4PhononTransFast.hh"
+#include "G4PhononTransSlow.hh"
 #include "G4SystemOfUnits.hh"
-#include "G4PhysicalConstants.hh"
 
 
-using namespace std;
-
-ChargePrimaryGeneratorAction::ChargePrimaryGeneratorAction()
-{
-  G4cout<<"\n\nChargePrimaryGeneratorAction::Constructor: running"<<endl;
+ChargePrimaryGeneratorAction::ChargePrimaryGeneratorAction() {
   G4int n_particle = 1;
   particleGun  = new G4ParticleGun(n_particle);
   
@@ -31,21 +25,16 @@ ChargePrimaryGeneratorAction::ChargePrimaryGeneratorAction()
   particleGun->SetParticleEnergy(DBL_MIN);
 }
 
-ChargePrimaryGeneratorAction::~ChargePrimaryGeneratorAction()
-{
+ChargePrimaryGeneratorAction::~ChargePrimaryGeneratorAction() {
   delete particleGun;
 }
 
-void ChargePrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
-{
-  for(int i=0;i<10;i++)
-  { 
-    particleGun->SetParticlePosition(G4ThreeVector(0.0,0.0,0.0));    
-    particleGun->SetParticleEnergy(1e-13*eV);  
-    particleGun->SetParticleDefinition(G4CMPDriftElectron::Definition());
-    particleGun->GeneratePrimaryVertex(anEvent);
-    particleGun->SetParticleDefinition(G4CMPDriftHole::Definition());
-    particleGun->GeneratePrimaryVertex(anEvent);
-  }
+void ChargePrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent) {
+  particleGun->SetParticlePosition(G4ThreeVector(0.0,0.0,0.0));    
+  particleGun->SetParticleEnergy(1e-13*eV);  
+  particleGun->SetParticleDefinition(G4CMPDriftElectron::Definition());
+  particleGun->GeneratePrimaryVertex(anEvent);
+  particleGun->SetParticleDefinition(G4CMPDriftHole::Definition());
+  particleGun->GeneratePrimaryVertex(anEvent);
 }
 
