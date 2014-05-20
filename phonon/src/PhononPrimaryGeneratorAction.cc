450fea94ca7459c3a016e001454f6e7e5a690183
Michael Kelsey <kelsey@slac.stanford.edu>
2014-05-19 21:34:00 -0700

    Allow setting phonon type via macro; use geantino to make density-of-states choice.

--- phonon/src/PhononPrimaryGeneratorAction.cc
+++ phonon/src/PhononPrimaryGeneratorAction.cc
@@ -28,67 +28,57 @@
 //
 // $Id$
 //
+// 20140519  Allow the user to specify phonon type by name in macro; if
+//	     "geantino" is set, use random generator to select.
 
 #include "PhononPrimaryGeneratorAction.hh"
 
 #include "G4Event.hh"
+#include "G4Geantino.hh"
 #include "G4ParticleGun.hh"
 #include "G4RandomDirection.hh"
-
 #include "G4PhononTransFast.hh"
 #include "G4PhononTransSlow.hh"
 #include "G4PhononLong.hh"
-
 #include "G4SystemOfUnits.hh"
 
 using namespace std;
 
-PhononPrimaryGeneratorAction::PhononPrimaryGeneratorAction()
-{ 
+PhononPrimaryGeneratorAction::PhononPrimaryGeneratorAction() { 
   G4int n_particle = 1;
   fParticleGun  = new G4ParticleGun(n_particle);   
 
-  // default particle kinematic
-  fParticleGun->SetParticleDefinition(G4PhononLong::PhononDefinition());
+  // default particle kinematics ("geantino" triggers random phonon choice)
+  fParticleGun->SetParticleDefinition(G4Geantino::Definition());
   fParticleGun->SetParticleMomentumDirection(G4RandomDirection());
   fParticleGun->SetParticlePosition(G4ThreeVector(0.0,0.0,0.0));
-  fParticleGun->SetParticleEnergy(1e-4*eV);  
+  fParticleGun->SetParticleEnergy(0.0075*eV);  
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
 
-PhononPrimaryGeneratorAction::~PhononPrimaryGeneratorAction()
-{
+PhononPrimaryGeneratorAction::~PhononPrimaryGeneratorAction() {
   delete fParticleGun;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
 
  
-void PhononPrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
-{
- 
-  fParticleGun->SetParticleMomentumDirection(G4RandomDirection());
-
-  G4double selector = G4UniformRand();
-  if(selector<0.53539) {
-         fParticleGun->SetParticleDefinition(G4PhononTransSlow::PhononDefinition()); 
-       }
-       else if(selector<0.90217) {
-         fParticleGun->SetParticleDefinition(G4PhononTransFast::PhononDefinition());
-       }
-       else {
-         fParticleGun->SetParticleDefinition(G4PhononLong::PhononDefinition());
+void PhononPrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent) {
+  if (fParticleGun->GetParticleDefinition() == G4Geantino::Definition()) {
+    G4double selector = G4UniformRand();
+    if (selector<0.53539) {
+      fParticleGun->SetParticleDefinition(G4PhononTransSlow::Definition()); 
+    } else if (selector<0.90217) {
+      fParticleGun->SetParticleDefinition(G4PhononTransFast::Definition());
+    } else {
+      fParticleGun->SetParticleDefinition(G4PhononLong::Definition());
+    }
   }
 
-  //Set phonon energy.
-  //Do not set momentum direction here.
-  //Any momentum direction set here will be overwritten
-  //by XPhononStackingAction::ClassifyNewTrack
-  fParticleGun->SetParticleEnergy(0.0075*eV);
+  fParticleGun->SetParticleMomentumDirection(G4RandomDirection());
   fParticleGun->GeneratePrimaryVertex(anEvent);
-
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
