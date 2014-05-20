1fd16bdd0220535fa6551a30b36431300e7dd414
Michael Kelsey <kelsey@slac.stanford.edu>
2014-05-19 21:03:20 -0700

    Drop setting of energy and position in GeneratePrimaries; macro commands usable.

--- charge/src/ChargePrimaryGeneratorAction.cc
+++ charge/src/ChargePrimaryGeneratorAction.cc
@@ -2,15 +2,12 @@
 //
 // Generator uses G4ParticleGun, producing one electron and one hole per
 // event by default.  User may change the number of particles per event
-// via macro command |/gun/number|
+// via macro command |/gun/number|, the starting position, or the energy.
 
 #include "ChargePrimaryGeneratorAction.hh"
 #include "G4CMPDriftElectron.hh"
 #include "G4CMPDriftHole.hh"
 #include "G4ParticleGun.hh"
-#include "G4PhononLong.hh"
-#include "G4PhononTransFast.hh"
-#include "G4PhononTransSlow.hh"
 #include "G4SystemOfUnits.hh"
 
 
@@ -20,9 +17,9 @@ ChargePrimaryGeneratorAction::ChargePrimaryGeneratorAction() {
   
   // default particle kinematic
   particleGun->SetParticleDefinition(G4CMPDriftElectron::Definition());
-  particleGun->SetParticleMomentumDirection(G4ThreeVector(1,0,0));
+  particleGun->SetParticleMomentumDirection(G4ThreeVector(0,0,1));
   particleGun->SetParticlePosition(G4ThreeVector(0.0,0.0,0.0));
-  particleGun->SetParticleEnergy(DBL_MIN);
+  particleGun->SetParticleEnergy(1e-13*eV);
 }
 
 ChargePrimaryGeneratorAction::~ChargePrimaryGeneratorAction() {
@@ -30,8 +27,6 @@ ChargePrimaryGeneratorAction::~ChargePrimaryGeneratorAction() {
 }
 
 void ChargePrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent) {
-  particleGun->SetParticlePosition(G4ThreeVector(0.0,0.0,0.0));    
-  particleGun->SetParticleEnergy(1e-13*eV);  
   particleGun->SetParticleDefinition(G4CMPDriftElectron::Definition());
   particleGun->GeneratePrimaryVertex(anEvent);
   particleGun->SetParticleDefinition(G4CMPDriftHole::Definition());
