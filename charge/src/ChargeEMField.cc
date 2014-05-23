22cc5f4ed80aa48655a31d5f63f6b2c1ea8d776b
Michael Kelsey <kelsey@slac.stanford.edu>
2014-05-22 21:29:14 -0700

    Move E-field handler to library, remove CDMS DMC z-offset from Epot_iZip4 files (now center is 0,0,0)

--- charge/src/ChargeEMField.cc
+++ charge/src/ChargeEMField.cc
@@ -2,16 +2,17 @@
 //
 // 20140324  Field is local to specified volume
 // 20140331  Do not pass lattice to filed manager; done track-by-track
+// 20140522  Migrate to general purpose (in name) field mesh
 
 #include "ChargeEMField.hh"
-#include "CDMS_iZip4_Field.hh"
+#include "G4CMPMeshElectricField.hh"
 #include "G4CMPFieldManager.hh"
 #include "G4LogicalVolume.hh"
 
 
 ChargeEMField::ChargeEMField(G4LogicalVolume* logVol) {
-  CDMS_iZip4_Field* fEMfield  = new CDMS_iZip4_Field("Epot_iZip4_small");
-  G4FieldManager*   fFieldMgr = new G4CMPFieldManager(fEMfield);
+  G4ElectricField* fEMfield  = new G4CMPMeshElectricField("Epot_iZip4_small");
+  G4FieldManager*  fFieldMgr = new G4CMPFieldManager(fEMfield);
 
   fFieldMgr->SetDetectorField(fEMfield);
   logVol->SetFieldManager(fFieldMgr, true);
---
+++ library/src/G4CMPMeshElectricField.cc
@@ -0,0 +1,102 @@
+// $Id$
+//
+// Wrapper class to process a numerically tabulated electric field mesh
+// and use QHull to interpolate the potential and field at arbitrary
+// points in the envelope of the mesh.  The input file format is fixed:
+// each line consists of four floating-point values, x, y and z in meters,
+// and voltage in volts.
+
+#include "G4CMPMeshElectricField.hh"
+#include "G4SystemOfUnits.hh"
+#include <vector>
+#include <fstream>
+
+using std::vector;
+
+
+G4CMPMeshElectricField::G4CMPMeshElectricField(const G4String& EpotFileName)
+  : G4ElectricField() {
+  BuildInterp(EpotFileName);
+}
+
+G4CMPMeshElectricField::G4CMPMeshElectricField(const G4CMPMeshElectricField &p)
+  : G4ElectricField(p), Interp(p.Interp) {;}
+
+G4CMPMeshElectricField& G4CMPMeshElectricField::operator=(const G4CMPMeshElectricField &p) {
+  if (this != &p) {				// Only copy if not self
+    G4ElectricField::operator=(p);		// Call through to base
+    Interp = p.Interp;
+  }
+
+  return *this;
+}
+
+
+void G4CMPMeshElectricField::BuildInterp(const G4String& EpotFileName) {
+#ifdef G4CMP_DEBUG
+  G4cout << "G4CMPMeshElectricField::Constructor: Creating Electric Field " 
+	 << EpotFileName << G4endl;
+#endif
+
+  vector<vector<G4double> > tempX;
+
+  vector<G4double> temp(4, 0);
+  G4double x,y,z,v;
+
+  std::ifstream epotFile(EpotFileName);
+  while (epotFile.good() && !epotFile.eof())
+  {
+    epotFile >> x >> y >> z >> v;
+    temp[0] = x*m;
+    temp[1] = y*m;
+    temp[2] = z*m;
+    temp[3] = v*volt;
+    tempX.push_back(temp);
+  }
+  epotFile.close();
+
+  std::sort(tempX.begin(),tempX.end(), CDMS_Efield::vector_comp);
+
+  vector<vector<G4double> > X(tempX.size(),vector<G4double>(3,0));
+  vector<G4double> V(tempX.size(),0);
+
+  for (size_t ii = 0; ii < tempX.size(); ++ii)
+  {
+    X[ii][0] = tempX[ii][0];
+    X[ii][1] = tempX[ii][1];
+    X[ii][2] = tempX[ii][2];
+    V[ii] = tempX[ii][3];
+  }
+
+  Interp.UseMesh(X, V);
+}
+
+
+void G4CMPMeshElectricField::GetFieldValue(const G4double Point[4],
+				     G4double *Efield) const {
+  Interp.GetField(Point,Efield);
+}
+
+
+G4double GetPotential(const G4double Point[4]) const {
+  return Interp.GetPotential(Point);
+}
+
+
+G4bool CDMS_Efield::vector_comp(const vector<G4double>& p1,
+				const vector<G4double>& p2) {
+  if (p1[0] < p2[0])
+    return true;
+  else if (p2[0] < p1[0])
+    return false;
+  else if (p1[1] < p2[1])
+    return true;
+  else if (p2[1] < p1[1])
+    return false;
+  else if (p1[2] < p2[2])
+    return true;
+  else if (p2[2] < p1[2])
+    return false;
+  else
+    return false;
+}
