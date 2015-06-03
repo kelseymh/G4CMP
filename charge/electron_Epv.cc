4532daa3a60a0d17b34f66c681954982c18a993e
Michael Kelsey <kelsey@slac.stanford.edu>
2015-06-03 14:28:07 -0700

    Modify output to show full K, V, and P vectors.  Old output preserved in commented-out blocks.

--- charge/electron_Epv.cc
+++ charge/electron_Epv.cc
@@ -29,11 +29,14 @@ int main(int argc, char* argv[]) {
 
   // Buffers for computations inside loops
   G4double kel, Ekin, meff, vtrue, veff, vG4Track, EoverM;
-  G4ThreeVector k_elec, p_elec;
+  G4ThreeVector k_elec, p_elec, v_elec, kdir, pdir, vdir;
 
   // Set up output file for import to Excel
+  /*****
   cout << "kmag\t\tk/ks\tp(eV)\tth(deg)\t E(ueV)\tmeff\tv(km/s)\tveff\tvG4"
        << endl;
+  *****/
+  cout << "Kx\tKy\tKz\tPx\tPy\tPz\tVx\tVy\tVz" << endl;
 
   // Loop over wavevectors up to "Mach 4"
   for (G4double mach=0.05; mach<=4.; mach += 0.05) {
@@ -41,38 +44,53 @@ int main(int argc, char* argv[]) {
 
     // Loop over angles relative to valley
     for (G4double thdeg=0; thdeg<=90.; thdeg += 5.) {
-      k_elec.setRThetaPhi(kel, thdeg*deg, 0.);
-      p_elec = lattice->MapK_valleyToP(1, k_elec);
+      for (G4double phideg=0; phideg<360.; phideg += 5.) {
+	if (phideg > 0. && thdeg==0.) break;
 
-      // Use momentum and valley to get energy, effective mass
-      Ekin = lattice->MapPtoEkin(1, p_elec);
-      meff = lattice->GetElectronEffectiveMass(1, p_elec);
+	k_elec.setRThetaPhi(kel, thdeg*deg, phideg*deg);
+	p_elec = lattice->MapK_valleyToP(1, k_elec);
+	v_elec = lattice->MapPtoV_el(1, p_elec);
 
-      vtrue = lattice->MapPtoV_el(1, p_elec).mag();	// True velocity
-      veff = p_elec.mag() / meff / c_light;		// Non-relativistic
-
-      // Compute velocity by copying G4Track method
-      EoverM = Ekin/meff/c_squared;
-      if (EoverM > velTable->GetMaxTOfVelocityTable()) {
-	vG4Track = c_light;
-      } else if (EoverM < DBL_MIN) {
-	vG4Track =0.;
-      } else if (EoverM < velTable->GetMinTOfVelocityTable()) {
-	vG4Track = c_light*std::sqrt(EoverM*(EoverM+2.))/(EoverM+1.0);
-      } else {    
-	vG4Track = velTable->Value(EoverM);
-      }
-
-      // Report kinematics
-      cout << fixed
-	   << setw(8) << setprecision(2) << kel << "\t" << mach
-	   << "\t" << setprecision(4) << p_elec.mag()/(1e-6*MeV)
-	   << "\t" << setw(2) << setprecision(0) << thdeg
-	   << "\t" << setw(7) << setprecision(3) << Ekin*1e12/MeV
-	   << "\t" << setprecision(5) << meff*c_squared/electron_mass_c2
-	   << "\t" << setprecision(4) << vtrue/(km/s)
-	   << "\t" << setprecision(4) << veff/(km/s)
-	   << "\t" << setprecision(4) << vG4Track/(km/s) << endl;
+	// Use momentum and valley to get energy, effective mass
+	Ekin = lattice->MapPtoEkin(1, p_elec);
+	meff = lattice->GetElectronEffectiveMass(1, p_elec);
+	
+	vtrue = v_elec.mag();			// True velocity
+	veff = p_elec.mag() / meff / c_light;	// Non-relativistic
+	
+	// Compute velocity by copying G4Track method
+	EoverM = Ekin/meff/c_squared;
+	if (EoverM > velTable->GetMaxTOfVelocityTable()) {
+	  vG4Track = c_light;
+	} else if (EoverM < DBL_MIN) {
+	  vG4Track =0.;
+	} else if (EoverM < velTable->GetMinTOfVelocityTable()) {
+	  vG4Track = c_light*std::sqrt(EoverM*(EoverM+2.))/(EoverM+1.0);
+	} else {    
+	  vG4Track = velTable->Value(EoverM);
+	}
+	
+	// Report kinematics
+	/*****
+	      cout << fixed
+	      << setw(8) << setprecision(2) << kel << "\t" << mach
+	      << "\t" << setprecision(4) << p_elec.mag()/(1e-6*MeV)
+	      << "\t" << setw(2) << setprecision(0) << thdeg
+	      << "\t" << setw(7) << setprecision(3) << Ekin*1e12/MeV
+	      << "\t" << setprecision(5) << meff*c_squared/electron_mass_c2
+	      << "\t" << setprecision(4) << vtrue/(km/s)
+	      << "\t" << setprecision(4) << veff/(km/s)
+	      << "\t" << setprecision(4) << vG4Track/(km/s) << endl;
+	*****/
+	kdir = k_elec.unit();
+	pdir = p_elec.unit();
+	vdir = v_elec.unit();
+	cout << fixed << setprecision(4)
+	     << kdir.x() << "\t" << kdir.y() << "\t" << kdir.z()
+	     << "\t" << pdir.x() << "\t" << pdir.y() << "\t" << pdir.z()
+	     << "\t" << vdir.x() << "\t" << vdir.y() << "\t" << vdir.z()
+	     << endl;
+      }	/* phideg */
     }	/* thdeg */
   }	/* mach */
 }
