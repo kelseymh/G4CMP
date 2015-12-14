5ec43df09f812f2c09211155c81f9c8cd451dbc1
Rob Agnese <ragnese@phys.ufl.edu>
2015-12-14 15:38:37 -0500

    Add exceptions for file i/o

--- charge/src/ChargeElectrodeSensitivity.cc
+++ charge/src/ChargeElectrodeSensitivity.cc
@@ -23,15 +23,24 @@ ChargeElectrodeSensitivity::ChargeElectrodeSensitivity(G4String name)
 
 void ChargeElectrodeSensitivity::Initialize(G4HCofThisEvent *HCE)
 {
+  //Prepare output file.
+  if (output.is_open()) output.close();
+  output.open(G4CMPConfigManager::GetHitOutput(), std::ios_base::app);
+  if (!output.good()) {
+    G4ExceptionDescription msg;
+    msg << "Error opening output file, " << fileName << ".\n"
+        << "Will continue simulation.";
+    G4Exception("ChargeElectrodeSensitivity::Initialize", "Charge003",
+                JustWarning, msg);
+    output.close();
+  } else {
+    output << "Run ID,Event ID,Track ID,Charge,Start Energy [eV],"
+           << "Track Lifetime [ns],Energy Deposit [eV],Start X [m],Start Y [m],"
+           << "Start Z [m],End X [m],End Y [m],End Z [m]"
+           << G4endl;
+  }
   //Call base class initialization.
   G4CMPElectrodeSensitivity::Initialize(HCE);
-
-  //Prepare output file.
-  output.open(G4CMPConfigManager::GetHitOutput(), std::ios_base::app);
-  output << "Run ID,Event ID,Track ID,Charge,Start Energy [eV],Track Lifetime [ns],"
-         << "Energy Deposit [eV],Start X [m],Start Y [m],Start Z [m],"
-         << "End X [m],End Y [m],End Z [m]"
-         << G4endl;
 
   //Initialize FETSim.
   if (FET->FETSimIsEnabled())
@@ -40,7 +49,14 @@ void ChargeElectrodeSensitivity::Initialize(G4HCofThisEvent *HCE)
 
 ChargeElectrodeSensitivity::~ChargeElectrodeSensitivity()
 {
-  output.close();
+  if (output.is_open()) output.close();
+  if (!output.good()) {
+    G4ExceptionDescription msg;
+    msg << "Error closing output file, " << fileName << ".\n"
+        << "Expect bad things like loss of data.";
+    G4Exception("ChargeElectrodeSensitivity::~ChargeElectrodSensitivity",
+                "Charge004", JustWarning, msg);
+  }
   delete FET;
 }
 
@@ -50,18 +66,24 @@ void ChargeElectrodeSensitivity::EndOfEvent(G4HCofThisEvent* HCE)
         static_cast<G4CMPElectrodeHitsCollection*>(HCE->GetHC(GetHCID()));
   std::vector<G4CMPElectrodeHit*>* hitVec = hitCol->GetVector();
   std::vector<G4CMPElectrodeHit*>::iterator itr = hitVec->begin();
-  for (; itr != hitVec->end(); itr++)
-    output << G4RunManager::GetRunManager()->GetCurrentRun()->GetRunID() << ','
-           << G4RunManager::GetRunManager()->GetCurrentEvent()->GetEventID() << ','
-           << (*itr)->GetTrackID() << ',' << (*itr)->GetCharge() << ','
-           << (*itr)->GetStartEnergy()/eV << ',' << (*itr)->GetFinalTime()/ns
-           << ',' << (*itr)->GetEnergyDeposit()/eV << ','
-           << (*itr)->GetStartPosition().getX()/m << ','
-           << (*itr)->GetStartPosition().getY()/m << ','
-           << (*itr)->GetStartPosition().getZ()/m << ','
-           << (*itr)->GetFinalPosition().getX()/m << ','
-           << (*itr)->GetFinalPosition().getY()/m << ','
-           << (*itr)->GetFinalPosition().getZ()/m << G4endl;
+  G4RunManager* runMan = G4RunManager::GetRunManager();
+  if (output.good()) {
+    for (; itr != hitVec->end(); itr++) {
+      output << runMan->GetCurrentRun()->GetRunID() << ','
+             << runMan->GetCurrentEvent()->GetEventID() << ','
+             << (*itr)->GetTrackID() << ','
+             << (*itr)->GetCharge() << ','
+             << (*itr)->GetStartEnergy()/eV << ','
+             << (*itr)->GetFinalTime()/ns << ','
+             << (*itr)->GetEnergyDeposit()/eV << ','
+             << (*itr)->GetStartPosition().getX()/m << ','
+             << (*itr)->GetStartPosition().getY()/m << ','
+             << (*itr)->GetStartPosition().getZ()/m << ','
+             << (*itr)->GetFinalPosition().getX()/m << ','
+             << (*itr)->GetFinalPosition().getY()/m << ','
+             << (*itr)->GetFinalPosition().getZ()/m << G4endl;
+    }
+  }
   FET->Digitize();
 }
 
