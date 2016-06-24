06b3914841e98667092ad650ac1752ea9de35d39
Rob Agnese <ragnese@phys.ufl.edu>
2016-06-24 17:42:28 -0400

    Make G4LatticeReader get LatticeDir from G4CMPConfigManager so macro command actually does something

--- library/src/G4CMPConfigManager.cc
+++ library/src/G4CMPConfigManager.cc
@@ -39,7 +39,7 @@ G4CMPConfigManager::G4CMPConfigManager()
     ehBounces(getenv("G4CMP_EH_BOUNCES")?atoi(getenv("G4CMP_EH_BOUNCES")):1),
     pBounces(getenv("G4CMP_PHON_BOUNCES")?atoi(getenv("G4CMP_PHON_BOUNCES")):100),
     Epot_file(getenv("G4CMP_EPOT_FILE")?getenv("G4CMP_EPOT_FILE"):"Epot_iZip4_small"),
-    LatticeDir(getenv("G4LATTICEDATA")?getenv("G4LATTICEDATA"):"."),
+    LatticeDir(getenv("G4LATTICEDATA")?getenv("G4LATTICEDATA"):"./CrystalMaps"),
     Hit_file(getenv("G4CMP_HIT_FILE")?getenv("G4CMP_HIT_FILE"):"epositions.txt"),
     messenger(new G4CMPConfigMessenger(this)) {;}
 
