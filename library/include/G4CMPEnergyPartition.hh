a05a12779f6b0c92b5823d953faa1ff3b9982ff0
Michael Kelsey <kelsey@slac.stanford.edu>
2016-08-25 21:30:14 -0700

    G4CMP-4 : New class implementing conversion of energy deposit to phonons and charge carriers.

---
+++ library/include/G4CMPEnergyPartition.hh
@@ -0,0 +1,83 @@
+/***********************************************************************\
+ * This software is licensed under the terms of the GNU General Public *
+ * License version 3 or later. See G4CMP/LICENSE for the full license. *
+\***********************************************************************/
+
+/// \file library/include/G4CMPEnergyPartition.hh
+/// \brief Definition of the G4CMPEnergyPartition class
+///   Functionality to convert energy deposition from Geant4 (both total
+///   and non-ionizing) into phonons and charge carrier pairs
+///
+// $Id$
+//
+
+#include "globals.hh"
+#include "G4ThreeVector.hh"
+#include <vector>
+
+class G4DynamicParticle;
+class G4LatticePhysical;
+class G4Material;
+class G4PrimaryParticle;
+class G4ParticleDefinition;
+
+
+class G4CMPEnergyPartition : public G4CMPProcessUtils {
+public:
+  G4CMPEnergyPartition(G4Material* mat=0, G4LatticePhysical* lat=0);
+  virtual ~G4CMEnergyPartition();
+
+  // Material is needed for (Z,A) in Lindhard scaling
+  void SetMaterial(G4Material* mat) { material = mat; }
+
+  // Set debugging output
+  void SetVerboseLevel(G4int vb) { verboseLevel = vb; }
+
+  // Nuclear recoil deposit uses Lindhard scale factor for e/h vs. phonons
+  void NuclearRecoil(G4double energy) {
+    G4double lind = LindhardScalingFactor(G4double energy);
+    DoPartition(energy*lind, energy*(1.-lind));
+  }
+
+  // Pure ionization produces no phonons
+  void Ionization(G4double energy) { DoPartition(energy, 0.); }
+
+  // Some processes can specify non-ionizing energy directly
+  void DoPartition(G4double eIon, G4double eNIEL);
+
+  // Return either primary or secondary particles from partitioning
+  void GetPrimaries(std::vector<G4PrimaryParticle*>& primaries) const;
+  void GetSecondaries(std::vector<G4DynamicParticle*>& secondaries) const;
+  
+  // Fraction of total energy deposit in material which goes to e/h pairs
+  G4double LindhardScalingFactor(G4double energy) const;
+
+  // Portion of ionization energy which goes to e/h pairs (Fano factor)
+  G4double MeasuredChargeEnergy(G4double eTrue) const;
+
+protected:
+  void GenerateCharges(G4double energy);
+  void GeneratePhonons(G4double energy);
+
+protected:
+  G4Material* material;		// To get (Z,A) for Lindhard scaling
+  G4double holeFraction;	// Energy from e/h pair taken by hole (50%)
+  G4int verboseLevel;		// Higher numbers give more details
+
+  size_t nPairs;		// Estimated number of pairs to produce
+  G4double chargeEnergyLeft;	// Energy to partition into e/h pairs
+
+  size_t nPhonons;		// Estimated number of phonons to produce
+  G4double phononEnergyLeft;	// Energy to partition into phonons
+
+  struct Data {
+    G4ParticleDefintion* partDef;
+    G4ThreeVector dir;
+    G4double ekin;
+
+    Data(G4ParticleDefintion* pd, const G4ThreeVector& d, G4double E)
+      : partDef(pd), dir(d), ekin(E) {;}
+  };
+    
+  std::vector<Data> particles;	// Combined phonons and charge carriers
+};
---
+++ library/src/G4CMPEnergyPartition.cc
@@ -0,0 +1,197 @@
+/***********************************************************************\
+ * This software is licensed under the terms of the GNU General Public *
+ * License version 3 or later. See G4CMP/LICENSE for the full license. *
+\***********************************************************************/
+
+/// \file library/src/G4CMPEnergyPartition.cc
+/// \brief Implementation of the G4CMPEnergyPartition class
+///   Functionality to convert energy deposition from Geant4 (both total
+///   and non-ionizing) into phonons and charge carrier pairs
+///
+// $Id$
+//
+
+#include "G4CMPEnergyPartition.hh"
+#include "G4CMPConfigManager.hh"
+#include "G4DynamicParticle.hh"
+#include "G4LatticePhysical.hh"
+#include "G4Material.hh"
+#include "G4Pow.hh"
+#include "G4PrimaryParticle.hh"
+#include "G4SystemOfUnits.hh"
+#include "Randomize.hh"
+#include <cmath>
+#include <vector>
+
+
+// Constructors and destructor
+
+G4CMPEnergyPartition::G4CMPEnergyPartition(G4Material* mat,
+					   G4LatticePhysical* lat)
+  : G4CMPProcessUtils(), material(mat), holeFraction(0.5),
+    verboseLevel(G4ConfigManager::GetVerboseLevel()),
+    nPairs(0), chargeEnergyLeft(0.), nPhonons(0), phononEnergyLeft(0.) {
+  SetLattice(lat);
+}
+
+G4CMPEnergyPartition::~G4CMEnergyPartition() {;}
+
+
+// Fraction of total energy deposit in material which goes to e/h pairs
+
+G4double G4CMPEnergyPartition::LindhardScalingFactor(G4double E) const {
+  if (!material) {
+    static const G4bool report = true;
+    if (report) {
+      G4cerr << "G4CMPEnergyPartition: No material configured" << G4endl;
+      report = false;
+    }
+    return 1.;
+  }
+
+  static const G4Pow* g4pow = G4Pow::GetInstance();	// Tabulated for speed
+  const G4double Z=material->GetZ(), A=material->GetA();
+
+  if (verboseLevel>1) {
+    G4cout << "G4CMPEnergyPartition::LindhardScalingFactor " << E
+	   << " for (Z,A) " << Z << " " << A << G4endl;
+  }
+
+  // From Lewin and Smith, 1996
+  G4double epsilon = 0.0115 * E * g4pow->powN(g4pow->A13(Z),-7);
+  G4double k = 0.133 * g4pow->A23(Z) / std::sqrt(A);
+  G4double h = (0.7*g4pow->powA(epsilon,0.6) + 3.*g4pow->powA(epsilon,0.15)
+		+ epsilon);
+
+  if (verboseLevel>2) {
+    G4cout << " eps " << epsilon << " k " << k << " h " << h << " : "
+	   << 1.-1./(k*h) << G4endl;
+  }
+
+  return (1. - 1./(k*h));
+}
+
+
+// Apply Fano factor to convert true energy deposition to random pairs
+
+G4double G4CMPEnergyPartition::MeasuredChargeEnergy(G4double eTrue) const {
+  // Fano noise changes the measured charge energy
+  // Std deviation of energy distribution
+  G4double sigmaE = std::sqrt(eTrue * lattice->GetFanoFactor()
+			      lattice->GetPairProductionEnergy());
+  return G4RandGauss::shoot(eTrue, sigmaE);
+}
+
+// Generate charge carriers and phonons according to uniform phase space
+
+void G4CMPEnergyPartition::DoPartition(G4double eIon, G4double eNIEL) {
+  if (verboseLevel) {
+    G4cout << "G4CMPEnergyPartition::DoPartition eIon " << eIon/MeV
+	   << " eNIEL " << eNIEL/MeV << " MeV" << G4endl;
+  }
+
+  particles.clear();		// Discard previous results
+
+  GenerateCharges(eIon);
+  GeneratePhonons(eNIEL + chargeEnergyLeft);
+}
+
+void G4CMPEnergyPartition::GenerateCharges(G4double energy) {
+  if (verboseLevel>1)
+    G4cout << "G4CMPEnergyPartition::GenerateCharges " << energy << G4endl;
+
+  G4double ePair = lattice->GetPairProductionEnergy();
+  G4double eMeas = MeasuredChargeEnergy(energy);	// Applies Fano factor
+
+  nPairs = std::floor(eMeas / ePair);		// Average number of e/h pairs
+  particles.reserve(particles.size() + 2*nPairs);
+
+  if (verboseLevel>2)
+    G4cout << " eMeas " << eMeas/MeV << " MeV => " << nPairs << G4endl;
+
+  chargeEnergyLeft = eMeas;
+  while (chargeEnergyLeft > ePair) {
+    AddChargePair(ePair);
+    chargeEnergyLeft -= ePair;
+  }
+
+  if (verboseLevel>2) G4cout << " " << chargeEnergyLeft << " excess" << G4endl;
+}
+
+void G4CMPEnergyPartition::AddChargePair(G4double ePair) {
+  G4double eFree = ePair - lattice->GetBandGapEnergy();	// TODO: Is this right?
+
+  charges.push_back(Data(G4CMPDriftElectron::Definition(), G4RandomDirection(),
+			 (1.-holeFraction)*eFree));
+
+  charges.push_back(Data(G4CMPDriftHole::Definition(), G4RandomDirection(),
+			 holeFraction*eFree));
+}
+
+void G4CMPEnergyPartition::GeneratePhonons(G4double energy) {
+  if (verboseLevel>1)
+    G4cout << "G4CMPEnergyPartition::GeneratePhonons " << energy << G4endl;
+
+  G4double ePhon = lattice->GetDebyeEnergy();	// TODO: No fluctuations yet!
+
+  nPhonons = std::ceil(energy / ePhon);		// Average number of phonons
+  particles.reserve(particles.size() + nPhonons);
+
+  if (verboseLevel>2)
+    G4cout << " ePhon " << ePhon/eV << " eV => " << nPhonons << G4endl;
+
+  phononEnergyLeft = energy;
+  while (phononEnergyLeft > ePhon) {
+    AddPhonon(ePhon);
+    phononEnergyLeft -= ePhon;
+  }
+
+  if (phononEnergyLeft > 0.) AddPhonon(phononEnergyLeft);	// Residual
+}
+
+void G4CMPEnergyPartition::AddPhonon(G4double ePhon) {
+  G4ParticleDefinition* pd =
+    G4PhononPolarization::Get(ChoosePhononPolarization());
+
+  particles.push_back(Data(pd, G4RandomDirection(), ePhon));
+}
+
+
+// Return either primary or secondary particles from partitioning
+
+void G4CMPEnergyPartition::
+GetPrimaries(std::vector<G4PrimaryParticle*>& primaries) const {
+  if (verboseLevel) G4cout << "G4CMPEnergyPartition::GetPrimaries" << G4endl;
+
+  primaries.clear();
+  primaries.resize(particles.size());	// Pre-create list, address by index
+
+  if (verboseLevel>1) G4cout << " filling " << particles.size() << G4endl;
+
+  for (size_t i=0; i<particles.size(); i++) {
+    const Data& p = particles[i];	// For convenience below
+    primaries[i] = new G4PrimaryParticle();
+    primaries[i]->SetParticleDefinition(p.pd);
+    primaries[i]->SetMomentumDirection(p.dir);
+    primaries[i]->SetKineticEnergy(p.ekin);
+
+    if (verboseLevel>2) primaries[i].Print();
+  }
+}
+
+void G4CMPEnergyPartition::
+GetSecondaries(std::vector<G4DynamicParticle*>& secondaries) const {
+  if (verboseLevel) G4cout << "G4CMPEnergyPartition::GetSecondaries" << G4endl;
+
+  secondaries.clear();
+  secondaries.resize(particles.size());	// Pre-create list, address by index
+
+  if (verboseLevel>1) G4cout << " filling " << particles.size() << G4endl;
+
+  for (size_t i=0; i<particles.size(); i++) {
+    const Data& p = particles[i];	// For convenience below
+    secondaries[i] = new G4DynamicParticle(p.pd, p.dir, p.ekin);
+
+    if (verboseLevel>2) secondaries[i]->DumpInfo();
+  }
+}
