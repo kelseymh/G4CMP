ccfbf8bd92bab0f5d329a8bed5359830dfd1725d
Michael Kelsey <kelsey@slac.stanford.edu>
2017-07-27 12:00:43 -0700

    Fix neutral impurities expression; add comment questioning D_op magnitudes.

--- library/src/G4CMPIVScatteringPhysical.cc
+++ library/src/G4CMPIVScatteringPhysical.cc
@@ -50,32 +50,29 @@ G4double
 G4CMPIVScatteringPhysical::GetMeanFreePath(const G4Track& aTrack,
 					    G4double,
 					    G4ForceCondition* condition) {
- *condition = NotForced;
- 
+  *condition = NotForced;
 
- G4double velocity = GetVelocity(aTrack);
+  // Track kinematics
+  G4double velocity = GetVelocity(aTrack);
   G4double energy = GetKineticEnergy(aTrack);
 
-
+  // Crystal properties (will want to get from lattice, material, etc.)
   G4double T = 0.015*kelvin ;
   G4double mass_electron = electron_mass_c2/c_squared;  
   G4double ml = 1.38 *mass_electron;
   G4double mt = .081 *mass_electron;
- 
- //Acoustic Phonon Scattering 
-  G4double M_D =cbrt( ml*mt*mt)* kilogram ;    
-  G4double D_ac = 11*eV;            
-  G4double rho = 5.327e3 * kilogram/m3 ; 		
-  G4double alpha=0.3/eV;                     
 
-  
- 
- 
-
-  // Useful constants for expressions below
+  const G4double M_D = cbrt(ml*mt*mt);    	// Average carrier mass
+  const G4double M_D3half = sqrt(M_D*M_D*M_D);
   const G4double hbar_sq  = hbar_Planck * hbar_Planck;
   const G4double hbar_4th = hbar_sq * hbar_sq;
-  const G4double M_D3half = sqrt(M_D*M_D*M_D);
+ 
+  //Acoustic Phonon Scattering 
+  G4double D_ac  = 11*eV;            
+  G4double rho   = 5.327e3 * kilogram/m3; 		
+  G4double alpha = 0.3/eV;                     
+
+  // Useful constants for expressions below
   const G4double D_ac_sq = D_ac*D_ac;
   const G4double alpha_times_energy = alpha*energy;
   const G4double velocity_sq = velocity*velocity;
@@ -83,58 +80,61 @@ G4CMPIVScatteringPhysical::GetMeanFreePath(const G4Track& aTrack,
   G4double amfp =(sqrt(2)*k_Boltzmann * T * M_D3half * D_ac_sq *
 		  sqrt(energy + alpha_times_energy *energy) * (1+ 2*alpha_times_energy))/
                  (pi* hbar_4th * rho * velocity_sq);
-  
+
+  if (verboseLevel>1)
+    G4cout << " Acoustic phonon Scattering " <<  amfp << G4endl;
 
   // Optical phonon Scattering equation
-  G4double D_op []= { 3e10*eV, 2e9*eV }  ;
-  G4double w_op []= { 27.3e-3*eV, 10.3e-3*eV } ;
-  G4double omfp[] = {0,0};
+  G4double D_op[] = { 3e10*eV, 2e9*eV };	// Are these values correct?
+  G4double w_op[] = { 27.3e-3*eV, 10.3e-3*eV };
+  G4double omfp[] = { 0,0 };
   G4double omfpTotal = 0;
   
   for (int i = 0; i<2; i++) {
-    G4double hw_op = hbar_Planck * w_op[i];// Energy of optical Phonon
+    G4double hw_op = hbar_Planck * w_op[i];	// Energy of optical Phonon
     G4double energy_minus_hw_op = energy - hw_op;
     G4double D_op_sq = D_op[i]*D_op[i];
     G4double alpha_times_ehw_op = alpha * energy_minus_hw_op;
-    G4double everything_under_sqrt = sqrt(energy_minus_hw_op + energy_minus_hw_op * alpha_times_ehw_op);
-
+    G4double everything_under_sqrt =
+      sqrt(energy_minus_hw_op + energy_minus_hw_op * alpha_times_ehw_op);
 
     omfp[i] =  k_Boltzmann * T * M_D3half * D_op_sq * everything_under_sqrt *
       (1 + 2*alpha_times_ehw_op) / (sqrt(2) * pi* hbar_sq *rho*hw_op);
-   
+
+    if (verboseLevel>1)
+      G4cout << " Optical phonon Scattering " << i << " " << omfp[i] << G4endl;
+
     omfpTotal+=omfp[i];
   }
+
+  if (verboseLevel>1)
+    G4cout << " Optical Phonon Scattering " << omfpTotal << G4endl;
  
   //Neutral Impurities
   G4double epsilon_r = 16.2;
   G4double epsilon = epsilon_r * epsilon0;	       
   G4double e_r =  epsilon0/epsilon;
   G4double E_T = (M_D /mass_electron) * e_r;
-  G4double n_l = 1e17 ;			// Units? The number density of inpurities
+  G4double n_l = 1e17 ;		// Units? The number density of inpurities
   
-  G4double Gamma = (4*sqrt(2)* n_l * hbar_sq * sqrt(energy))/
-    ( sqrt(m*m*m)* (energy + E_T));
-  
-  
+  G4double Gamma = (4*sqrt(2)* n_l * hbar_sq * sqrt(energy)) /
+    ( M_D3half * (energy + E_T));
+
+  if (verboseLevel>1)
+    G4cout << " Neutral Impurities " << Gamma << G4endl;
   
   G4double mfp  =  velocity / ( Gamma + omfpTotal + amfp ); 
-
   
-  
-  if (verboseLevel > 1) {
-    G4cout << "IV MFP = " << mfp/m << " m"
-	   << "\n this is Acoustic phonon Scattering " <<  amfp
-	   << "\n this is the Optical Phonon Scattering " << omfpTotal
-	   << "\n this Neutral Impurities " << Gamma << G4endl;
-  }
+  if (verboseLevel>1) G4cout << "IV MFP = " << mfp/m << " m" << G4endl;
 
   return mfp; 
    
 }
 
 
-G4VParticleChange*  G4CMPIVScatteringPhysical::PostStepDoIt(const G4Track& aTrack, 
-							    const G4Step& aStep) {
+G4VParticleChange*
+G4CMPIVScatteringPhysical::PostStepDoIt(const G4Track& aTrack, 
+					const G4Step& aStep) {
   aParticleChange.Initialize(aTrack); 
   G4StepPoint* postStepPoint = aStep.GetPostStepPoint();
   
