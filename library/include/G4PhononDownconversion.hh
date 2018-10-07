7a8a88f3922a7a4ea9fa8579e591ac7fad1916d4
Jyotirmai Singh <joe.singh@berkeley.edu>
2018-10-06 23:24:04 -0700

    Remove process utility functions

--- library/include/G4PhononDownconversion.hh
+++ library/include/G4PhononDownconversion.hh
@@ -19,35 +19,22 @@ class G4PhononDownconversion : public G4VPhononProcess {
 public:
   G4PhononDownconversion(const G4String& processName ="phononDownconversion");
   virtual ~G4PhononDownconversion();
-
   virtual G4VParticleChange* PostStepDoIt(const G4Track&, const G4Step& );
 
-  // Only longitudinal phonons decay (L -> L' T, or L -> T T') 
+  // Only longitudinal phonons decay (L -> L' T, or L -> T T')
   virtual G4bool IsApplicable(const G4ParticleDefinition&);
-  
+
 protected:
   // Keep function here as call-back to avoid getting old toolkit version
   virtual G4double GetMeanFreePath(const G4Track& trk, G4double prevstep,
 				   G4ForceCondition* cond) {
     return G4CMPVProcess::GetMeanFreePath(trk, prevstep, cond);
   }
-  
-private:
-  // relative probability that anharmonic decay occurs L->L'+T'
-  inline double GetLTDecayProb(G4double, G4double) const;
-  inline double GetTTDecayProb(G4double, G4double) const;
-  inline double MakeLDeviation(G4double, G4double) const;
-  inline double MakeTTDeviation(G4double, G4double) const;
-  inline double MakeTDeviation(G4double, G4double) const;
 
-  void MakeTTSecondaries(const G4Track&);
-  void MakeLTSecondaries(const G4Track&);
 
 private:
-  G4double fBeta, fGamma, fLambda, fMu;	// Local buffers for calculations
-  G4double fvLvT;			// Ratio of sound speeds
 
-  // hide assignment operator as private 
+  // hide assignment operator as private
   G4PhononDownconversion(G4PhononDownconversion&);
   G4PhononDownconversion& operator=(const G4PhononDownconversion& right);
 
