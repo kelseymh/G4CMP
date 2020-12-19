b942415949edc903dd6af48022728516d8257cea
Michael Kelsey <kelsey@slac.stanford.edu>
2020-12-18 17:50:03 -0800

    G4CMP-235 : Revise G4CMPFanoBinomial: change arguments to Ntrue and Fano factor.  Modify interpolation to use probability (1-F) directly everywhere. Improve Choose() and Binomial PDF calculations to avoid loss of precision. Apply 3-sigma "cut" to replace binmial throws with simpler Gaussian.

--- library/src/G4CMPFanoBinomial.cc
+++ library/src/G4CMPFanoBinomial.cc
@@ -7,13 +7,20 @@
 
 // =======================================================================
 // 20201019  Michael Kelsey (TAMU)
+// 20201213  Replace "stdDev" argument with Fano factor
+// 20201218  Improve computing steps to avoid loss of precision, apply
+//	       cutoff for binomial -> Gaussian limit.
 // =======================================================================
 
 #include "G4CMPFanoBinomial.hh"
 #include "CLHEP/Random/DoubConv.h"
 #include "CLHEP/Random/RandBinomial.h"
+#include "CLHEP/Random/RandGaussQ.h"
+#include "CLHEP/Random/RandPoissonQ.h"
 #include <algorithm>	// for min() and max()
+#include <cfloat>	// for DBL_MAX
 #include <cmath>	// for exp()
+#include <iostream>
 
 using CLHEP::HepRandomEngine;
 using CLHEP::DoubConv;
@@ -25,25 +32,25 @@ HepRandomEngine & FanoBinomial::engine() {return *localEngine;}
 FanoBinomial::~FanoBinomial() {;}
 
 void FanoBinomial::shootArray( const int size, double* vect,
-                            double mean, double stdDev )
+                            double mean, double fano )
 {
   for( double* v = vect; v != vect+size; ++v )
-    *v = shoot(mean,stdDev);
+    *v = shoot(mean,fano);
 }
 
 void FanoBinomial::shootArray( HepRandomEngine* anEngine,
                             const int size, double* vect,
-                            double mean, double stdDev )
+                            double mean, double fano )
 {
   for( double* v = vect; v != vect+size; ++v )
-    *v = shoot(anEngine,mean,stdDev);
+    *v = shoot(anEngine,mean,fano);
 }
 
 void FanoBinomial::fireArray( const int size, double* vect,
-                           double mean, double stdDev )
+                           double mean, double fano )
 {
   for( double* v = vect; v != vect+size; ++v )
-    *v = fire(mean,stdDev);
+    *v = fire(mean,fano);
 }
 
 
@@ -53,47 +60,74 @@ void FanoBinomial::fireArray( const int size, double* vect,
 // https://www.slac.stanford.edu/exp/cdms/ScienceResults/DataReleases/20190401_HVeV_Run1/HVeV_R1_Data_Release_20190401.pdf
 
 double FanoBinomial::genBinomial( HepRandomEngine *anEngine, double mean,
-				  double stdDev ) {
+				  double fano ) {
   if (mean <= 0.) return 0.;		// Spread is ignored for zero binomial
   if (mean <= 1.) return 1.;		// Single quantized result, no spread
 
-  double fano = stdDev*stdDev / mean;
-  double prob = 1. - fano;
+  // Fano factor of 1. means Poisson distribution
+  if (fano == 1.) return CLHEP::RandPoissonQ::shoot(anEngine, mean);
 
-  double ntry = mean/prob;		// For binomial, this must be integer
+  double prob = 1. - fano;
+  double ntry = mean/prob;
+
+  // Use Gaussian approximation where appropriate
+  if (mean > 9*(fano/prob) && mean > 9*(prob/fano))
+    return CLHEP::RandGaussQ::shoot(anEngine, mean, sqrt(mean*fano));
+
+  // If integer, then nlo==nhi and no interpolation is needed
+  if (ntry == int(ntry)) {
+    return CLHEP::RandBinomial::shoot(anEngine, long(ntry), prob);
+  }
+
+  // Implement interpolated binomial distribution
   long nlo = std::floor(ntry);		
   long nhi = std::ceil(ntry);
 
-  double Flo = mean/nlo;
-  double Fhi = mean/nhi;
-  double dF = (fano - Flo)/(Fhi - Flo);
+  double Plo = mean/nlo;
+  double Phi = mean/nhi;
+  double dP = (prob - Plo)/(Phi - Plo);
 
   // Accept-reject loop to pick a result with interpolated probability
-  double pick = -1., pickP = 0.;
-  do {
-    pick = CLHEP::RandBinomial::shoot(anEngine, nhi, prob);
+  double pdfMax = pdfBinomial(long(nhi*prob), nlo, Plo);
 
-    pickP = ( pdfBinomial(pick, nlo, 1.-Flo)*(1.-dF)
-	      + pdfBinomial(pick, nhi, 1.-Fhi)*dF );
-  } while (anEngine->flat() < pickP);
+  long pick;
+  double pickP;
+  do {
+    pick = long(anEngine->flat()*(nhi+1));
+    //*** pick = CLHEP::RandBinomial::shoot(anEngine, nhi, prob);
+
+    pickP = ( pdfBinomial(pick, nlo, Plo)*(1.-dP)
+	      + pdfBinomial(pick, nhi, Phi)*dP );
+    pickP /= pdfMax;
+  } while (anEngine->flat() > pickP);
 
   return pick;
 }
 
 double FanoBinomial::pdfBinomial(long x, long n, double p) {
-  double lnbin = x*log(p) + (n-x)*log(1-p);
+  if (x > n || x < 0)  return 0.;		// Non-physical arguments
+  if (p <= 0. || p>1.) return 0.;
+  if (p == 1.) return (x==n ? 1. : 0.);		// Delta function at n
+
+  double lnbin = log(p)*x + log(1.-p)*(n-x);	// p^x * (1-p)^(n-x)
+
+  // Upper and lower limits for exp(), not defined anywhere else?
+  const double DBL_EXPARG_MAX=709.7, DBL_EXPARG_MIN=-708.3;
+  if (lnbin < DBL_EXPARG_MIN) return 0.;
+  if (lnbin > DBL_EXPARG_MAX) return DBL_MAX;
+
   return Choose(n,x) * exp(lnbin);
 }
 
-long FanoBinomial::Choose(long n, long x) {
-  if (x<=0)  return 1L;
-  if (x==1)  return n;
-  if (x>n/2) return Choose(n, n-x);	// Symmetry reduces looping
+double FanoBinomial::Choose(long n, long x) {
+  if (x>n/2) x = n-x;			// Symmetry reduces looping
+  if (x<0)  return 0.;			// Non-physical
+  if (x==0) return 1.;			// Simple cases avoid computation
+  if (x==1) return n;
 
-  long choose = 1;
-  for (long k=1; k<x; k++) {
-    choose *= n-k+1;		// Separate actions ensures good factors
-    choose /= k;
+  double choose = 1.;
+  for (long k=1L; k<=x; k++) {
+    choose *= double(n-k+1L)/k;
   }
   return choose;
 }
@@ -106,8 +140,8 @@ std::ostream & FanoBinomial::put ( std::ostream & os ) const {
   os << "Uvec" << "\n";
   t = DoubConv::dto2longs(defaultMean);
   os << defaultMean << " " << t[0] << " " << t[1] << std::endl;
-  t = DoubConv::dto2longs(defaultStdDev);
-  os << defaultStdDev << " " << t[0] << " " << t[1] << std::endl;
+  t = DoubConv::dto2longs(defaultFano);
+  os << defaultFano << " " << t[0] << " " << t[1] << std::endl;
   os.precision(pr);
   return os;
 }
@@ -116,6 +150,7 @@ std::istream & FanoBinomial::get ( std::istream & is ) {
   std::string inName;
   is >> inName;
   if (inName != name()) {
+
     is.clear(std::ios::badbit | is.rdstate());
     std::cerr << "Mismatch when expecting to read state of a "
     	      << name() << " distribution\n"
@@ -125,12 +160,12 @@ std::istream & FanoBinomial::get ( std::istream & is ) {
   }
   if (CLHEP::possibleKeywordInput(is, "Uvec", defaultMean)) {
     std::vector<unsigned long> t(2);
-    is >> defaultMean >> t[0]>>t[1]; defaultMean = DoubConv::longs2double(t);
-    is >> defaultStdDev>>t[0]>>t[1]; defaultStdDev = DoubConv::longs2double(t);
+    is >> defaultMean >> t[0] >> t[1]; defaultMean = DoubConv::longs2double(t);
+    is >> defaultFano >> t[0] >> t[1]; defaultFano = DoubConv::longs2double(t);
     return is;
   }
   // is >> defaultMean encompassed by possibleKeywordInput
-  is >> defaultStdDev;
+  is >> defaultFano;
 
   return is;
 }
