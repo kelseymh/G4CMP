d071e00a94ad34acc1b7b482f4288482ec78b55b
Michael Kelsey <kelsey@slac.stanford.edu>
2022-08-21 20:37:20 -0700

    Create G4CMPStepInfo container for use with hit merging without G4Steps.

--- library/include/G4CMPStepAccumulator.hh
+++ library/include/G4CMPStepAccumulator.hh
@@ -12,6 +12,7 @@
 // 20210608  Ensure that all values are initialized in constructor; add
 //	       eventID to avoid rollover between events.
 // 20220216  Add "stepID" to provide full identification.  Add printout.
+// 20220821  G4CMP-308 -- Define step-info container to avoid needing G4Step
 
 #ifndef G4CMPStepAccumulator_hh
 #define G4CMPStepAccumulator_hh 1
@@ -24,31 +25,49 @@ class G4ParticleDefinition;
 class G4Step;
 
 
-class G4CMPStepAccumulator {
+class G4CMPStepInfo {
 public:
-  G4CMPStepAccumulator() { Clear(); }
+  G4CMPStepInfo() : trackID(-1), stepID(-1), pd(0), Edep(0.), Eniel(0.) {;}
+  G4CMPStepInfo(const G4Step* step);
+  G4CMPStepInfo(const G4Step& step);
+  G4CMPStepInfo& operator=(const G4CMPStepInfo& step);
+
+  virtual void Clear() {
+    trackID = stepID = -1;
+    pd = 0; Edep = Eniel = 0.;
+    start.set(0,0,0); end.set(0,0,0);
+  }
+
+public:
+  G4int trackID;		  // Track ID for sanity checking in Add()
+  G4int stepID;		  	  // Step ID of last step accumulated
+  const G4ParticleDefinition* pd; // Particle type (should match all steps)
+  G4double Edep;		  // Sum of GetTotalEnergyDeposit()
+  G4double Eniel;		  // Sum of GetNonIonizingEnergyDeposit()
+  G4ThreeVector start;	  	  // PreStep position of first hit
+  G4ThreeVector end;		  // PostStep position of last hit
+
+};
+
+class G4CMPStepAccumulator : public G4CMPStepInfo {
+public:
+  G4CMPStepAccumulator() : G4CMPStepInfo(), nsteps(0), eventID(-1) {;}
   ~G4CMPStepAccumulator() {;}
 
   // Extract relevant information from step
-  void Add(const G4Step& step);
+  void Add(const G4CMPStepInfo& step);
+  void Add(const G4Step& step) { Add(G4CMPStepInfo(step)); }
   void Add(const G4Step* step) { if (step) Add(*step); }
 
   // Reset accumulator for new track
-  void Clear();
+  virtual void Clear() { G4CMPStepInfo::Clear(); nsteps=0; eventID=-1; }
 
   // Dump content for diagnostics
   void Print(std::ostream& os) const;
 
-public:		// Data values are public for more convenient access
-  G4int eventID;		  // Event ID for sanity checking in Add()
-  G4int trackID;		  // Track ID for sanity checking in Add()
-  G4int stepID;			  // Step ID of last step accumulated
-  G4int nsteps;			  // Accumulated steps so far
-  const G4ParticleDefinition* pd; // Particle type (should match all steps)
-  G4double Edep;		  // Sum of GetTotalEnergyDeposit()
-  G4double Eniel;		  // Sum of GetNonIonizingEnergyDeposit()
-  G4ThreeVector start;		  // PreStep position of first hit
-  G4ThreeVector end;		  // PostStep position of last hit
+public:
+  G4int nsteps;			// Accumulated steps so far
+  G4int eventID;		// Current event ID for sanity checking in Add()
 };
 
 inline 
--- library/src/G4CMPStepAccumulator.cc
+++ library/src/G4CMPStepAccumulator.cc
@@ -13,6 +13,7 @@
 //	       rollover errors for track or event changes.
 // 20220216  Add "stepID" to provide full identification.  Add printout.
 // 20220228  Add sanity check that only energy-deposit hits are accumulated.
+// 20220821  G4CMP-308 -- Define step-info container to avoid needing G4Step
 
 #include "globals.hh"
 #include "G4CMPStepAccumulator.hh"
@@ -26,45 +27,68 @@
 #include <iostream>
 
 
-// Reset accumulator for new track
+// Populate step-info container
 
-void G4CMPStepAccumulator::Clear() {
-  eventID = trackID = stepID = -1;
-  nsteps = 0;
-  pd = 0;
-  Edep = Eniel = 0.;
-  start.set(0,0,0);
-  end.set(0,0,0);
+G4CMPStepInfo::G4CMPStepInfo(const G4Step* step)
+  : trackID(step->GetTrack()->GetTrackID()),
+    stepID(step->GetTrack()->GetCurrentStepNumber()),
+    pd(step->GetTrack()->GetDefinition()),
+    Edep(step->GetTotalEnergyDeposit()),
+    Eniel(step->GetNonIonizingEnergyDeposit()),
+    start(step->GetPreStepPoint()->GetPosition()),
+    end(step->GetPostStepPoint()->GetPosition()) {;}
+
+G4CMPStepInfo::G4CMPStepInfo(const G4Step& step)
+  : trackID(step.GetTrack()->GetTrackID()),
+    stepID(step.GetTrack()->GetCurrentStepNumber()),
+    pd(step.GetTrack()->GetDefinition()),
+    Edep(step.GetTotalEnergyDeposit()),
+    Eniel(step.GetNonIonizingEnergyDeposit()),
+    start(step.GetPreStepPoint()->GetPosition()),
+    end(step.GetPostStepPoint()->GetPosition()) {;}
+
+// Duplicate information from specified other step
+
+G4CMPStepInfo&
+G4CMPStepInfo::operator=(const G4CMPStepInfo& step) {
+  trackID = step.trackID;
+  stepID = step.stepID;
+  pd = step.pd;
+  Edep = step.Edep;
+  Eniel = step.Eniel;
+  start = step.start;
+  end = step.end;
+
+  return *this;
 }
 
 
 // Extract relevant information from step
 
-void G4CMPStepAccumulator::Add(const G4Step& step) {
+void G4CMPStepAccumulator::Add(const G4CMPStepInfo& step) {
   // If event ID has changed discard previous data and report error
   G4int thisEvt = G4RunManager::GetRunManager()->GetCurrentEvent()->GetEventID();
   if (eventID >=0 && eventID != thisEvt) {
     G4cerr << "ERROR G4CMPStepAccumulator rolled over between events "
 	   << eventID << " and " << thisEvt << G4endl
-	   << " Energy " << Edep/eV << " eV, NIEL " << Eniel/eV << " eV"
+	   << " Energy " << Edep/eV << " eV,"
+	   << " NIEL " << Eniel/eV << " eV"
 	   << " lost from previous event." << G4endl;
     Clear();
   }
-  
+
   // If track type or track ID has changed, discard previous data
-  const G4Track* track = step.GetTrack();
-  if (trackID >= 0 && (trackID != track->GetTrackID() ||
-		       pd != track->GetParticleDefinition())) {
+  if (trackID >= 0 && (trackID != step.trackID || pd != step.pd)) {
     G4cerr << "ERROR G4CMPStepAccumulator rolled over between tracks "
-	   << trackID << " and " << track->GetTrackID() << G4endl
-           << " Energy " << Edep/eV << " eV, NIEL " << Eniel/eV << " eV"
+	   << trackID << " and " << step.trackID << G4endl
+           << " Energy " << Edep/eV << " eV,"
+	   << " NIEL " << Eniel/eV << " eV"
            << " lost from previous track." << G4endl;
     Clear();
   }
 
   // Should never receive zero-energy step
-  if (step.GetTotalEnergyDeposit() <= 0. &&
-      step.GetNonIonizingEnergyDeposit() <= 0.) {
+  if (step.Edep <= 0. && step.Eniel <= 0.) {
     G4cerr << "ERROR G4CMPStepAccumulator received zero-energy step."
 	   << G4endl;
     return;
@@ -73,26 +97,21 @@ void G4CMPStepAccumulator::Add(const G4Step& step) {
   // Initialize new accumulation
   if (nsteps == 0) {
     eventID = thisEvt;
-    trackID = track->GetTrackID();
-    pd = track->GetParticleDefinition();
-    start = step.GetPreStepPoint()->GetPosition();
+    (G4CMPStepInfo)(*this) = step;
   }
 
   // Update step information
   nsteps++;
-  stepID = track->GetCurrentStepNumber();
+  stepID = step.stepID;		// Step ID will always point to last step
 
   // Compute energy-weighted centroid of step endpoints
-  G4double stepEdep = step.GetTotalEnergyDeposit();
-  const G4ThreeVector& stepEnd = step.GetPostStepPoint()->GetPosition();
-
-  ((end *= Edep) += stepEnd*stepEdep) /= (Edep+stepEdep);
+  ((end *= Edep) += step.end*step.Edep) /= (Edep+step.Edep);
   // NOTE: Using accumulators above as lvalues to avoid creating temporaries
   // Equivalent to: end = (end*Edep + stepEnd*stepEdep)/(Edep+stepEdep);
   
   // Accumulate total energy of steps
-  Edep  += step.GetTotalEnergyDeposit();
-  Eniel += step.GetNonIonizingEnergyDeposit();
+  Edep  += step.Edep;
+  Eniel += step.Eniel;
 }
 
 
@@ -103,6 +122,7 @@ void G4CMPStepAccumulator::Print(std::ostream& os) const {
      << " " << (pd?pd->GetParticleName():"")
      << " track " << trackID << "/" << stepID
      << "\n " << nsteps << " steps from " << start << " to " << end
-     << "\n deposited " << Edep/eV << " eV, " << Eniel/eV << " eV NIEL"
+     << "\n deposited " << Edep/eV << " eV, "
+     << Eniel/eV << " eV NIEL"
      << std::endl;
 }
