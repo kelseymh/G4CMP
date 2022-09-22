f6e7e6b36b1c55323965dfd6fe0ba729583a2883
Michael Kelsey <kelsey@slac.stanford.edu>
2022-09-21 17:00:12 -0700

    Add test of new thermal distribution.

---
+++ tests/testTemperature.cc
@@ -0,0 +1,98 @@
+/***********************************************************************\
+ * This software is licensed under the terms of the GNU General Public *
+ * License version 3 or later. See G4CMP/LICENSE for the full license. *
+\***********************************************************************/
+
+// Usage: testTemperature <T> <Material>
+//
+// Specify temperature (K) and lattice directory/material name.
+// Geant4 material will be set as "G4_<Material>".
+//
+// NOTE: Typical devices should be run at 0.1, 0.05, 0.02 K.
+//
+// 20220921  G4CMP-319 -- Exercise new temperature parameter and distribution.
+
+#include "globals.hh"
+#include "G4CMPConfigManager.hh"
+#include "G4CMPUtils.hh"
+#include "G4LatticeManager.hh"
+#include "G4LatticePhysical.hh"
+#include "G4LogicalVolume.hh"
+#include "G4NistManager.hh"
+#include "G4NistManager.hh"
+#include "G4PhysicalConstants.hh"
+#include "G4PVPlacement.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4Tubs.hh"
+#include <stdlib.h>
+
+
+// Global variables for use in tests
+
+namespace {
+  G4CMPConfigManager* g4cmp = G4CMPConfigManager::Instance();
+  G4LatticePhysical* lattice = 0;
+}
+
+
+// Construct lattice with a "fake" physical volume
+
+G4LatticePhysical* BuildLattice(const G4String& lname) {
+  G4String mname = "G4_"+lname;
+
+  // MUST USE 'new', SO THAT G4SolidStore CAN DELETE
+  G4Material* mat = G4NistManager::Instance()->FindOrBuildMaterial(mname);
+  G4Tubs* crystal = new G4Tubs(lname+"Crystal", 0., 5.*cm, 1.*cm, 0., 360.*deg);
+  G4LogicalVolume* lv = new G4LogicalVolume(crystal, mat, crystal->GetName());
+  G4PVPlacement* pv = new G4PVPlacement(0, G4ThreeVector(), lv, lv->GetName(),
+					0, false, 1);
+
+  lattice = G4LatticeManager::Instance()->LoadLattice(pv,lname);
+
+  return lattice;
+}
+
+
+// Main test is here
+
+int main(int argc, char* argv[]) {
+  if (argc < 3) {
+    G4cerr << "Usage: " << argv[0] << " <Temperature> <Material>" << G4endl
+	   << "\tTemperature should be in kelvins" << G4endl;
+    ::exit(1);
+  }
+
+  G4double temp = strtod(argv[1],NULL) * kelvin;
+  G4String lname = argv[2];
+  G4int verbose = (argc>3) ? atoi(argv[3]) : 0;
+
+  BuildLattice(lname);
+  lattice->SetTemperature(temp);
+  if (verbose) lattice->Dump(G4cout);
+
+  // Calculate a few points, make sure lattice temperature is set
+  G4double Etemp = k_Boltzmann*temp;
+  G4double pdfTemp = G4CMP::MaxwellBoltzmannPDF(temp, Etemp);
+  G4double pdfLat = G4CMP::MaxwellBoltzmannPDF(lattice->GetTemperature(), Etemp);
+  G4double pdfHalf = G4CMP::MaxwellBoltzmannPDF(temp, Etemp/2.);
+  G4double pdfDouble = G4CMP::MaxwellBoltzmannPDF(temp, Etemp*2.);
+  G4double pdfFive = G4CMP::MaxwellBoltzmannPDF(temp, Etemp*5.);
+  G4double pdfTen = G4CMP::MaxwellBoltzmannPDF(temp, Etemp*10.);
+
+  G4cout << "T = " << temp/kelvin << " K,"
+	 << " E = kT = " << Etemp/eV << " eV" << G4endl
+	 << "PDF(E)   " << pdfTemp << " from lattice " << pdfLat << G4endl
+	 << "PDF(E/2) " << pdfHalf << " PDF(2E) " << pdfDouble << G4endl
+	 << "PDF(5E)  " << pdfFive << " PDF(10E) " << pdfTen << G4endl;
+
+  // Estimate integral of PDF from 0. up to 20*kT, should be unity
+  G4double sum = 0., dE = Etemp/20.;
+  for (G4double Ei=0.; Ei<20.*Etemp; Ei+=dE) {
+    sum += G4CMP::MaxwellBoltzmannPDF(temp, Ei+dE/2.) / 20.;
+  }
+
+  G4cout << "Integral [0,20kT] = " << sum << G4endl;
+
+  // Result is success if equal PDFs and unit integral
+  return (pdfTemp == pdfLat && fabs(sum-1.) < 1e-6);
+}
