07647b7d83aa33b83fe6b347889519a1a2f739c1
Yasin Ferdous Alam <yasin.alam@login2.cluster>
2022-10-21 05:32:26 -0500

    Recombination Rate calculations with T dependence

---
+++ library/include/G4CMPQPRecombinationRate.hh
@@ -0,0 +1,23 @@
+/// \file library/include/G4CMPQPRecombinationRate.hh
+///// \brief Compute rate for quasiparticle scattering
+//
+
+#ifndef G4CMPQPRecombinationRate_hh
+#define G4CMPQPRecombinationRate_hh 1
+
+#include "G4CMPVScatteringRate.hh"
+
+class G4CMPQPRecombinationRate : public G4CMPVScatteringRate {
+public:
+  G4CMPQPRecombinationRate() : G4CMPVScatteringRate("QPRecombination") {;}
+  virtual ~G4CMPQPRecombinationRate() {;}
+
+
+  virtual G4double Rate(const G4Track& aTrack) const;
+  virtual G4double Rate(G4double energy, G4double temp=0) const;
+private:
+  //helper functions
+  G4double Recombination(G4double qp_E, G4double x, G4double T, G4double Tc, G4double Gap0, G4double b);
+  G4double Numeric_Integral(N_emm G4double qp_E,  G4double T, G4double Tc, G4double Gap0, G4double b);
+
+}
