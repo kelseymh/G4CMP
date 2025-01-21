942c4f8ee5845a01a059f6bbdd4a3cc0081ef482
Michael Kelsey <kelsey@slac.stanford.edu>
2025-01-21 09:21:56 -0800

    Add user argument for number of points to testNRyield.

--- tests/testNRyield.cc
+++ tests/testNRyield.cc
@@ -1,18 +1,20 @@
 // testNRyield: Exercise different Lindhard-yield (NIEL) functions
 //
-// Usage: testNRyield Emin Emax unit [Material = G4_Si]
+// Usage: testNRyield Emin Emax unit [Npoints=100] [Material = G4_Si]
 //
 // Arguments: Emin, Emax specify energy range to test, with the specified
-//            units (e.g., "1 10 keV").  Optional Material should be a
+//            units (e.g., "1 10 keV").  Npoints is number of points in
+//            energy range to sample.  Optional Material should be a
 //            Geant4 NIST name ("G4_something"), used to get both the
 //            projectile and target (Z,A) values.
 //
-// Code will generate 50 steps for the given energy range, and call each of
-// the available NIEL functions (this utility must be updated when new NIEL
-// functions are added to the library).  The ionization yield from each
-// function will be printed in a tab-delimited format suitable for plotting.
+// Code will call each of the available NIEL functions (this utility must be
+// updated when new NIEL functions are added to the library).  The ionization
+// yield from each function will be printed in a tab-delimited format suitable
+// for plotting.
 //
 // 20250102  Michael Kelsey
+// 20250121  Add argument for number of points
 
 #include "globals.hh"
 #include "G4CMPLewinSmithNIEL.hh"
@@ -25,12 +27,13 @@
 #include "G4UnitsTable.hh"
 #include "G4VNIELPartition.hh"
 #include <cmath>
+#include <iomanip>
 #include <float.h>
 #include <stdlib.h>
 
 
 void testNRyield(G4double Emin, G4double Emax, const G4String& unit,
-		 const G4String& material="G4_Si") {
+		 G4int nStep, const G4String& material="G4_Si") {
   // Make sure that units and material strings are valid
   G4bool goodInput = true;
   if (!(goodInput &= G4UnitDefinition::IsUnitDefined(unit)))
@@ -56,27 +59,24 @@ void testNRyield(G4double Emin, G4double Emax, const G4String& unit,
     new G4CMPSarkisNIEL, new G4CMPImpactTunlNIEL };
 
   // Output will be tab-delimited columns for all the NIEL functions
-  G4cout << "Energy";
-  for (size_t i=0; i<nNIEL; i++) G4cout << "\t" << useNIEL[i];
+  G4cout << std::left << "Energy";
+  for (size_t i=0; i<nNIEL; i++) G4cout << "\t" << std::setw(8) << useNIEL[i];
   G4cout << G4endl;
 
   // From material, Get Z and A for use as projectile (internal nuclear recoil)
   G4double Zin = target->GetZ(), Ain = target->GetA();
 
-  // Compute uniform logarithmic steps to see full range of yield values
-  const G4int nStep = 50;
-  const G4double Estep = (Emax-Emin)/nStep;
-
   // Loop over energy steps, get yield values at each energy
   G4double unitVal = G4UnitDefinition::GetValueOf(unit);
 
+  G4double Estep = (Emax-Emin)/nStep;
   for (G4int iStep=0; iStep<=nStep; iStep++) {	// Avoids cumulative rounding
     G4double E = Emin + iStep*Estep;
-    G4cout << E;
+    G4cout << std::setw(5) << E;
 
     for (size_t iNIEL=0; iNIEL<nNIEL; iNIEL++) {
       G4double Y =  NIELfunc[iNIEL]->PartitionNIEL(E*unitVal, target, Zin, Ain);
-      G4cout << "\t" << Y;
+      G4cout << "\t" << std::setw(8) << Y;
     }
     G4cout << G4endl;
   }
@@ -88,9 +88,10 @@ void testNRyield(G4double Emin, G4double Emax, const G4String& unit,
 int main(int argc, char* argv[]) {
   // Get required arguments, or report usage and exit
   if (argc < 4) {
-    G4cerr << "Usage: testNRyield Emin Emax unit [Material = G4_Si]\n\n"
+    G4cerr << "Usage: testNRyield Emin Emax unit [points=100] [Material=G4_Si]\n\n"
 	   << "Arguments: Emin, Emax: specify energy range to test\n"
 	   << "           units: Valid Geant4 unit string (e.g., 'MeV')\n"
+	   << "           points: Number of energy points to sample\n"
 	   << "           material: Valid Geant4 NIST name; default is G4_Si\n"
 	   << "\nCode generates tab-delimited table of steps in energy,\n"
 	   << "computing ionization yield Y from each of the NIEL-partition\n"
@@ -104,10 +105,11 @@ int main(int argc, char* argv[]) {
   G4double Emin = strtod(argv[1],0);
   G4double Emax = strtod(argv[2],0);
   G4String unit = argv[3];
-  G4String mat  = (argc>4) ? argv[4] : "G4_Si";
+  G4int points  = (argc>4) ? atoi(argv[4]) : 100;
+  G4String mat  = (argc>5) ? argv[5] : "G4_Si";
 
-  G4cout << "Nuclear recoils in " << mat << " " << Emin << "-" << Emax
-	 << " " << unit << G4endl;
+  G4cout << "Nuclear recoils in " << mat << " " << points << " steps: "
+	 << Emin << "-" << Emax << " " << unit << G4endl;
 
-  testNRyield(Emin, Emax, unit, mat);
+  testNRyield(Emin, Emax, unit, points, mat);
 }
