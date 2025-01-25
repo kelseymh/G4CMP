bf00267b6e8e78ea6113c055defdda61aa9b012f
Michael Kelsey <kelsey@slac.stanford.edu>
2025-01-24 21:56:06 -0800

    Use RunManager from Factory instead of forcing single-threaded RM.

--- examples/caustics/g4cmpPhononCaustics.cc
+++ examples/caustics/g4cmpPhononCaustics.cc
@@ -8,7 +8,7 @@
 //
 // $Id$
 //
-#include "G4RunManager.hh"
+#include "G4RunManagerFactory.hh"
 #include "G4UIExecutive.hh"
 #include "G4UImanager.hh"
 #include "G4VisExecutive.hh"
@@ -21,55 +21,48 @@
 
 int main(int argc,char** argv)
 {
- // Construct the run manager
- //
- G4RunManager * runManager = new G4RunManager;
+  // Construct the run manager
+  //
+  G4RunManager * runManager = G4RunManagerFactory::CreateRunManager();
 
- // Set mandatory initialization classes
- //
- Caustic_PhononDetectorConstruction* detector = new Caustic_PhononDetectorConstruction();
- runManager->SetUserInitialization(detector);
+  // Set mandatory initialization classes
+  //
+  Caustic_PhononDetectorConstruction* detector = new Caustic_PhononDetectorConstruction();
+  runManager->SetUserInitialization(detector);
+  
+  G4VUserPhysicsList* physics = new G4CMPPhysicsList();
+  physics->SetCuts();
+  runManager->SetUserInitialization(physics);
+  
+  // Set user action classes (different for Geant4 10.0)
+  //
+  runManager->SetUserInitialization(new Caustic_PhononActionInitialization);
+  
+  // Create configuration managers to ensure macro commands exist
+  G4CMPConfigManager::Instance();
+  Caustic_PhononConfigManager::Instance();
+  
+  // Visualization manager
+  //
+  G4VisManager* visManager = new G4VisExecutive;
+  visManager->Initialize();
+  
+  // This Part is to run the Macros
+  
+  G4UImanager* UImanager = G4UImanager::GetUIpointer();
+  
+  if (argc==1) {   // Define UI session for interactive mode
+    G4UIExecutive * ui = new G4UIExecutive(argc,argv);
+    ui->SessionStart();
+    delete ui;
+  } else {           // Batch mode
+    G4String command = "/control/execute ";
+    G4String fileName = argv[1];
+    UImanager->ApplyCommand(command+fileName);
+  }
 
- G4VUserPhysicsList* physics = new G4CMPPhysicsList();
- physics->SetCuts();
- runManager->SetUserInitialization(physics);
-
- // Set user action classes (different for Geant4 10.0)
- //
- runManager->SetUserInitialization(new Caustic_PhononActionInitialization);
-
- // Create configuration managers to ensure macro commands exist
- G4CMPConfigManager::Instance();
- Caustic_PhononConfigManager::Instance();
-
- // Visualization manager
- //
- G4VisManager* visManager = new G4VisExecutive;
- visManager->Initialize();
-
-// This Part is to run the Macros
-
- G4UImanager* UImanager = G4UImanager::GetUIpointer();
-
- if (argc==1)   // Define UI session for interactive mode
- {
-      G4UIExecutive * ui = new G4UIExecutive(argc,argv);
-      ui->SessionStart();
-      delete ui;
- }
- else           // Batch mode
- {
-   G4String command = "/control/execute ";
-   G4String fileName = argv[1];
-   UImanager->ApplyCommand(command+fileName);
- }
-
- delete visManager;
- delete runManager;
-
- return 0;
-
-
-
-
+  delete visManager;
+  delete runManager;
+  
+  return 0;
 }
